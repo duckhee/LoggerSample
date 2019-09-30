9f39f308c0c83d3ae4f1b1a951492da23cecd1ef
duckhee <fain930@yahoo.co>
2019-09-30 17:40:08 +0900

    set user main page

--- express/server/routes/user/user.router.js
+++ express/server/routes/user/user.router.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const UserCtrl = require('../../ctrl/user/user.ctrl');
 
+router.get('/', (req, res, next)=>{
+    res.render('user/index');
+});
+
 router.get('/login', UserCtrl.LoginPage);
 
 router.post('/login', UserCtrl.LoginDo);
