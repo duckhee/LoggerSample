2db15d478ad2797e5186fd9e6bc11c445f5838c9
duckhee <fain930@yahoo.co>
2019-11-26 17:13:28 +0900

    setting sedder test

--- loggerWeb/server/DataBase/seeders/20191118080721-site.js
+++ loggerWeb/server/DataBase/seeders/20191118080721-site.js
@@ -1,26 +1,38 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+    up: (queryInterface, Sequelize) => {
+        /*
+          Add altering commands here.
+          Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
-  },
+          Example:
+          return queryInterface.bulkInsert('People', [{
+            name: 'John Doe',
+            isBetaMember: false
+          }], {});
+        */
+        let dumpData = [];
+        let dumpTime = new Date();
+        for (let i = 0; i < 100; i++) {
+            let site = {
+                Owner: 'admin@co.kr',
+                name: 'testing' + i,
+                createdAt: dumpTime,
+                updatedAt: dumpTime
+            };
+            dumpData.push(site);
+        }
+        return queryInterface.bulkInsert('sites', dumpData, {});
+    },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
+    down: (queryInterface, Sequelize) => {
+        /*
+          Add reverting commands here.
+          Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
-  }
+          Example:
+          return queryInterface.bulkDelete('People', null, {});
+        */
+    }
 };
